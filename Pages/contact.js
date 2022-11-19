225bf1e1d811a7781184e68b5cd19166cc99e1c6
JavierOlomG <jolombradag@gmail.com>
2022-11-19 10:26:24 +0100

    WebTributo

---
+++ Pages/contact.js
@@ -0,0 +1,19 @@
+/*
+var nombre = document.getElementById("nombre");
+
+console.log(nombre);
+
+document.getElementById("boton").onclick = function (){
+    if (nombre == "Javi"){
+        alert("Rellene el nombre");
+        console.log(nombre);
+    }else{
+    alert("Formulario Enviado");
+    console.log(nombre);
+}
+};
+*/
+
+document.getElementById("boton").onclick = function(){
+    alert("Formulario Enviado");
+}
