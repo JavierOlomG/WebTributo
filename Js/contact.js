5489dbc75394b81bd584631579eea9b13413d479
JavierOlomG <jolombradag@gmail.com>
2022-11-21 13:47:50 +0100

    JS para formulario

--- Js/contact.js
+++ Js/contact.js
@@ -1,10 +1,14 @@
 document.getElementById("boton").onclick = function (){
     let nombre = document.getElementById("nombre").value;
     let email = document.getElementById("email").value;
+    let terms = document.getElementById("terms").checked;
+
     if (nombre == ""){
         alert("Rellene el nombre");
     }else if(email == ""){
         alert("Rellene el email");
+    }else if(terms == false){
+        alert("Acepte terminos y condiciones")
     }
     else{
     alert("Enhorabuena " +  nombre +" Formulario Enviado");
